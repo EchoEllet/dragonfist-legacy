0db603e7f419069fe540bd5e265c745096daec2f
Ellet <echo.ellet@gmail.com>
2025-12-22 16:38:26 +0300

    docs: document why NeoForge client entrypoint is not used

--- neoforge/src/main/kotlin/dev/echoellet/dragonfist_legacy/client/DragonFistLegacyNeoForgeClient.kt
+++ neoforge/src/main/kotlin/dev/echoellet/dragonfist_legacy/client/DragonFistLegacyNeoForgeClient.kt
@@ -8,6 +8,12 @@ import net.neoforged.neoforge.client.event.EntityRenderersEvent
 import net.neoforged.neoforge.client.gui.ConfigurationScreen
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory
 
+// Note: We tried using "@Mod(DragonFistLegacy.ID, Dist.CLIENT)",
+// however, in production side, the client entrypoint is called
+// before the common entrypoint, which is not an issue during development.
+//
+// Kotlin "lateinit var" is intentionally used, and client renderers try to access
+// entities, which are initialized in the common entrypoint.
 object DragonFistLegacyNeoForgeClient {
     fun initialize(modEventBus: IEventBus, container: ModContainer) {
         DragonFistLegacyClient.initialize(
